9af46714023422f3dafe0e0a7dbec74d038fab2d
Raj Kashid <117977675+Itsrajsk@users.noreply.github.com>
2023-09-23 21:53:53 +0530

    Pattern 2 Added

---
+++ Patterns/Pattern_02.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern1(int n)
+{
+    for (int i = 0; i<n; i++)
+    {
+        for (int  j = 0; j<=i; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+}
+
+int main()
+{
+    int t;  // This is For Testcases
+    cout<<"Enter Number How Many TestCases You Have To Do?"<<endl;
+    cin>>t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;  // Entering Rows
+        cout<<"Enter Rows: "<<endl;
+        cin>>n; 
+        pattern1(n);  // Calling Function
+    }
+    
+    return 0;
+}

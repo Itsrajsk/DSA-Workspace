b9af6dd474ffc6a171b8dcaee7a7b5a27fd2c31a
Raj Kashid <117977675+Itsrajsk@users.noreply.github.com>
2023-09-24 19:07:07 +0530

    Pattern 19 Added

---
+++ Patterns/Pattern_19.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern(int n)
+{
+    int initS = 0;
+    // 1st Half
+    for (int i = 0; i < n; i++)
+    {
+
+        // Stars
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+
+        // Spaces
+        for (int j = 0; j < initS; j++)
+        {
+            cout << " ";
+        }
+
+        // Stars
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+        initS += 2;
+        cout << endl;
+    }
+    // 2nd Half
+    initS=8;
+    for (int i = 1; i <=n; i++)
+    {
+        // Stars
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        // Spaces
+        for (int j = 0; j < initS; j++)
+        {
+            cout << " ";
+        }
+
+        // Stars
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        initS -= 2;
+        cout << endl;
+    }
+}
+int main()
+{
+    int t; // This is For Testcases
+    cout << "Enter Number How Many TestCases You Have To Do?" << endl;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n; // Entering Rows
+        cout << "Enter Rows: " << endl;
+        cin >> n;
+        pattern(n); // Calling Function
+    }
+
+    return 0;
+}

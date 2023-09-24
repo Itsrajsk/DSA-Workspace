2b21569bdeab067e0bb49d064c8a7944607acab0
Raj Kashid <117977675+Itsrajsk@users.noreply.github.com>
2023-09-24 14:11:14 +0530

    Pattern 13 Added

---
+++ Patterns/Pattern_13.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern(int n)
+{
+    int num =1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<num;
+            num+=1;
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    int t; // This is For Testcases
+    cout << "Enter Number How Many TestCases You Have To Do?" << endl;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n; // Entering Rows
+        cout << "Enter Rows: " << endl;
+        cin >> n;
+        pattern(n); // Calling Function
+    }
+
+    return 0;
+}

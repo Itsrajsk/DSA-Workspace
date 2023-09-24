e786078b05bf5c4e8fa315785644a8abcd0b64b9
Raj Kashid <117977675+Itsrajsk@users.noreply.github.com>
2023-09-24 13:50:13 +0530

    Pattern 10 Added

---
+++ Patterns/Pattern_10.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern(int n)
+{
+    for (int i = 1; i <= 2*n-1; i++)
+    {int stars=i;
+    if(i>n){
+        stars=2*n-i;
+    }
+        for (int j = 1; j <= stars; j++)
+        {
+            cout<<"*";
+        }
+        
+        cout<<endl;
+    }
+    
+    
+}
+int main()
+{
+    int t;  // This is For Testcases
+    cout<<"Enter Number How Many TestCases You Have To Do?"<<endl;
+    cin>>t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;  // Entering Rows
+        cout<<"Enter Rows: "<<endl;
+        cin>>n; 
+        pattern(n);  // Calling Function
+    }
+    
+    return 0;
+}

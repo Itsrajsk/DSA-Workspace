96c4237fa737245965d8e9542ca40ae878abf225
Raj Kashid <117977675+Itsrajsk@users.noreply.github.com>
2023-09-24 14:22:06 +0530

    Pattern 15 Added

---
+++ Patterns/Pattern_15.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern(int n)
+{int i=2;
+    for(int i=0;i<n;i++)
+    {
+        for(char ch='A';ch<='A'+(n-i-1);ch++)
+        {
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    int t; // This is For Testcases
+    cout << "Enter Number How Many TestCases You Have To Do?" << endl;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n; // Entering Rows
+        cout << "Enter Rows: " << endl;
+        cin >> n;
+        pattern(n); // Calling Function
+    }
+
+    return 0;
+}

d7bfa1c002492aadedf4c6460717780505655dd8
Raj Kashid <117977675+Itsrajsk@users.noreply.github.com>
2023-09-24 13:13:31 +0530

    Pattern 6 Added

---
+++ Patterns/Pattern_06.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern(int n)
+{
+    for (int i = 1; i<=n; i++)  // Outer Loops For Rows
+    {
+        for (int  j = 1; j<=n-i+1; j++) // Inner Loops For Printing The Rows Columnwise
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
+int main()
+{
+    int t;  // This is For Testcases
+    cout<<"Enter Number How Many TestCases You Have To Do?"<<endl;
+    cin>>t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;  // Entering Rows
+        cout<<"Enter Rows: "<<endl;
+        cin>>n; 
+        pattern(n);  // Calling Function
+    }
+    
+    return 0;
+}

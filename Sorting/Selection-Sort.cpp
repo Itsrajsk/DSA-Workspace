245e63a223c131904dce4f8ab6444cd543e8d40e
Raj Kashid <117977675+Itsrajsk@users.noreply.github.com>
2023-10-02 11:14:05 +0530

    Selection Sort Added

--- Sorting/Selection-Sort.cpp
+++ Sorting/Selection-Sort.cpp
@@ -1,8 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+int Selection_Sort(int arr[], int n)
+{
 
+    for (int i = 0; i <= n - 2; i++)
+    {
+        int min = i;
+        for (int j = i; j <= n - 1; j++)
+        {
+            if (arr[j] <= arr[min])
+            {
+                min = j;
+            }
+        }
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+}
 int main()
 {
-    
+    int n;
+    cout << "Array Size : " << endl;
+    cin >> n;
+    int arr[n];
+    cout << "Enter Array Numbers : " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    Selection_Sort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
